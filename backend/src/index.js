0178870596d20ab00291ff59b7d77989569e8f39
Mounika-0311 <mounika.mupparaju@archents.com>
2026-03-03 17:51:29 +0530

    db

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   'http://localhost:3000',
   'http://127.0.0.1:3000',
-  'https://farming-frontend-roan.vercel.app',
+  'https://farming-frontend-two.vercel.app',
   
 ];
 
